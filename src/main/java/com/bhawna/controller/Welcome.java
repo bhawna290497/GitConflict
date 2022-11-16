a46836c9fce73e86912a1f69e54c017f2ac60ab4
Bhawna <bhawna290497@gmail.com>
2022-11-16 14:32:15 +0530

    Project added to central Repository

---
+++ src/main/java/com/bhawna/controller/Welcome.java
@@ -0,0 +1,13 @@
+package com.bhawna.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class Welcome {
+
+	public String welcome()
+	{
+		return null;
+		
+	}
+}
